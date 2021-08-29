e8371915ddfa54966279764b13aee4c1c21fa0bf
Thomas Haebler <thomas@haebler.ch>
2021-08-29 16:50:06 +0200

    Chart version 1

--- src/app/seating-chart/seating-chart.component.ts
+++ src/app/seating-chart/seating-chart.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { SeatingService, Table } from '../seating.service';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart, registerables, TooltipItem, TooltipModel } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { SeatingOverview, SeatingService } from '../seating.service';
 
 @Component({
   selector: 'app-seating-chart',
   templateUrl: './seating-chart.component.html',
   styleUrls: ['./seating-chart.component.css']
 })
-export class SeatingChartComponent implements OnInit {
-  constructor(public seatingService: SeatingService) {
-    this.seatingService.tables.subscribe(this.renderTables);
-  }
+export class SeatingChartComponent implements AfterViewInit {
+  chart?: Chart;
 
-  ngOnInit(): void {}
+  @ViewChild('chart')
+  canvas!: ElementRef;
+
+  constructor(public seatingService: SeatingService) {}
+
+  ngAfterViewInit() {
+    Chart.register(...registerables, ChartDataLabels);
+    this.seatingService.state.subscribe((state) => this.renderChart(state));
+  }
 
   onStart(): void {
     this.seatingService.start();
@@ -21,7 +29,105 @@ export class SeatingChartComponent implements OnInit {
     this.seatingService.rotate();
   }
 
-  private renderTables(tables: Table[]) {
-    console.log('render tables', tables);
+  private renderChart(state: SeatingOverview) {
+    const ctx = this.canvas.nativeElement.getContext(
+      '2d'
+    ) as CanvasRenderingContext2D;
+    if (this.chart) {
+      this.chart.destroy();
+    }
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: state.dates.map((date) => {
+          if (date.tableNumber > 0) {
+            return 'Table ' + date.tableNumber;
+          }
+          return 'Break';
+        }),
+        datasets: [
+          {
+            label: 'Ladies',
+            data: state.dates.map((date) => date.lady?.age),
+            borderColor: '#ff6699',
+            backgroundColor: '#ff6699'
+          },
+          {
+            label: 'Men',
+            data: state.dates.map((date) => date.man?.age),
+            borderColor: '#3399ff',
+            backgroundColor: '#3399ff'
+          },
+          {
+            label: 'Age Difference',
+            data: state.dates.map((date) => {
+              if (date.man && date.lady) {
+                return Math.abs(date.man.age - date.lady.age);
+              }
+              return 0;
+            }),
+            borderColor: '#00cc66',
+            backgroundColor: '#00cc66'
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: {
+          duration: 0
+        },
+        plugins: {
+          tooltip: {
+            mode: 'index',
+            intersect: false,
+            callbacks: {
+              label: function (
+                this: TooltipModel<'bar'>,
+                tooltipItem: TooltipItem<'bar'>
+              ): string | string[] {
+                const lady = state.dates[tooltipItem.dataIndex].lady;
+                const man = state.dates[tooltipItem.dataIndex].man;
+                if (lady && tooltipItem.datasetIndex === 0) {
+                  return `${lady.firstName}: ${lady.age}`;
+                } else if (man && tooltipItem.datasetIndex === 1) {
+                  return `${man.firstName}: ${man.age}`;
+                } else if (lady && man && tooltipItem.datasetIndex === 2) {
+                  const dataset =
+                    this.dataPoints[tooltipItem.datasetIndex].dataset.data;
+                  return `Age difference: ${dataset[tooltipItem.dataIndex]}`;
+                }
+                return '';
+              }
+            }
+          },
+          datalabels: {
+            align: 'end',
+            anchor: 'end',
+            rotation: 270,
+            font: function (context) {
+              return {
+                size: 11,
+                weight: 'bold'
+              };
+            },
+            formatter: function (value, context) {
+              if (context.datasetIndex === 0) {
+                return state.dates[context.dataIndex].lady?.firstName;
+              } else if (context.datasetIndex === 1) {
+                return state.dates[context.dataIndex].man?.firstName;
+              }
+              return value;
+            }
+          }
+        },
+        scales: {
+          y: {
+            min: 0,
+            max: state.maxAge + 20
+          }
+        }
+      }
+    }) as Chart;
   }
 }
--- src/app/seating.service.ts
+++ src/app/seating.service.ts
@@ -13,21 +13,40 @@ export interface Participant {
   age: number;
 }
 
-export interface Table {
-  tableIndex: number;
-  lady: Participant;
+export interface Date {
+  tableNumber: number;
+  lady?: Participant;
   man?: Participant;
+}
+
+export interface SeatingOverview {
+  dates: Date[];
+  medianAgeMen: number;
+  medianAgeLadies: number;
+  minAge: number;
+  maxAge: number;
+  numberOfMissingMen: number;
+  numberOfMissingLadies: number;
+  dateNumber: number;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeatingService {
+  _state: SeatingOverview = {
+    dates: [],
+    medianAgeMen: 0,
+    medianAgeLadies: 0,
+    minAge: 0,
+    maxAge: 0,
+    numberOfMissingMen: 0,
+    numberOfMissingLadies: 0,
+    dateNumber: 0
+  };
   numberOfDates = new BehaviorSubject<number>(10);
   participants = new BehaviorSubject<Participant[]>([]);
-  tables = new BehaviorSubject<Table[]>([]);
-  medianAgeMen = new BehaviorSubject<number>(0);
-  medianAgeLadies = new BehaviorSubject<number>(0);
+  state = new BehaviorSubject<SeatingOverview>(this._state);
 
   private startPosition = 0;
   private position = 0;
@@ -39,10 +58,12 @@ export class SeatingService {
     gender: Gender.Male,
     age: 0
   };
-  private _tables: Table[] = [];
-  private numberOfMissingMen = 0;
-  private numberOfMissingLadies = 0;
-  private takingABreak: Participant[] = [];
+  private static missingLady: Participant = {
+    firstName: '',
+    lastName: '',
+    gender: Gender.Female,
+    age: 0
+  };
 
   constructor() {
     this.participants.subscribe(() => this.start());
@@ -51,6 +72,10 @@ export class SeatingService {
 
   public start() {
     if (this.participants.value.length === 0) {
+      if (this._state.dates.length) {
+        this._state.dates = [];
+        this.state.next(this._state);
+      }
       return;
     }
     this.position = this.startPosition = Math.ceil(
@@ -62,44 +87,72 @@ export class SeatingService {
     const menSortedByAge = this.participants.value
       .filter((p) => p.gender === Gender.Male)
       .sort(sortByAge);
-    this.medianAgeLadies.next(medianAge(ladiesSortedByAge));
-    this.medianAgeMen.next(medianAge(menSortedByAge));
-    this.numberOfMissingMen = ladiesSortedByAge.length - menSortedByAge.length;
-    this.numberOfMissingLadies = -this.numberOfMissingMen;
-    this._tables = ladiesSortedByAge.map((lady, index) => {
-      return {
-        lady,
-        tableIndex: index
-      };
-    });
-    if (this.numberOfMissingMen > 0) {
-      this.numberOfMissingLadies = 0;
-      if (this.medianAgeLadies > this.medianAgeMen) {
-        for (let i = 0; i < this.numberOfMissingMen; i++) {
-          menSortedByAge.push(SeatingService.missingMan);
+    this._state.medianAgeLadies = medianAge(ladiesSortedByAge);
+    this._state.medianAgeMen = medianAge(menSortedByAge);
+    this._state.numberOfMissingMen =
+      ladiesSortedByAge.length - menSortedByAge.length;
+    this._state.numberOfMissingLadies = -this._state.numberOfMissingMen;
+    this.ladyQueue = [];
+    this.manQueue = [];
+    if (this._state.numberOfMissingMen > 0) {
+      this._state.numberOfMissingLadies = 0;
+      if (this._state.medianAgeLadies > this._state.medianAgeMen) {
+        const men = [...menSortedByAge];
+        for (let i = 0; i < this._state.numberOfMissingMen; i++) {
+          men.push(SeatingService.missingMan);
         }
         enqueue(ladiesSortedByAge.reverse(), this.ladyQueue);
-        enqueue(menSortedByAge.reverse(), this.manQueue);
+        enqueue(men.reverse(), this.manQueue);
       } else {
-        for (let i = 0; i < this.numberOfMissingMen; i++) {
-          menSortedByAge.unshift(SeatingService.missingMan);
+        const men = [...menSortedByAge];
+        for (let i = 0; i < this._state.numberOfMissingMen; i++) {
+          men.unshift(SeatingService.missingMan);
         }
         enqueue(ladiesSortedByAge, this.ladyQueue);
-        enqueue(menSortedByAge, this.manQueue);
+        enqueue(men, this.manQueue);
       }
-    } else if (this.numberOfMissingLadies > 0) {
-      this.numberOfMissingMen = 0;
-      if (this.medianAgeMen > this.medianAgeLadies) {
-        enqueue(ladiesSortedByAge.reverse(), this.ladyQueue);
+    } else if (this._state.numberOfMissingLadies > 0) {
+      this._state.numberOfMissingMen = 0;
+      if (this._state.medianAgeMen > this._state.medianAgeLadies) {
+        const ladies = [...ladiesSortedByAge];
+        for (let i = 0; i < this._state.numberOfMissingLadies; i++) {
+          ladies.push(SeatingService.missingLady);
+        }
+        enqueue(ladies.reverse(), this.ladyQueue);
         enqueue(menSortedByAge.reverse(), this.manQueue);
       } else {
-        enqueue(ladiesSortedByAge, this.ladyQueue);
+        const ladies = [...ladiesSortedByAge];
+        for (let i = 0; i < this._state.numberOfMissingLadies; i++) {
+          ladies.unshift(SeatingService.missingLady);
+        }
+        enqueue(ladies, this.ladyQueue);
         enqueue(menSortedByAge, this.manQueue);
       }
     } else {
       enqueue(ladiesSortedByAge, this.ladyQueue);
       enqueue(menSortedByAge, this.manQueue);
     }
+
+    let tableNumber = 1;
+    this._state.dates = this.ladyQueue.map((lady) => {
+      if (lady === SeatingService.missingLady) {
+        return {
+          tableNumber: -1
+        };
+      }
+      return {
+        lady,
+        tableNumber: tableNumber++
+      };
+    });
+    this._state.minAge = Math.min(
+      menSortedByAge[0].age,
+      ladiesSortedByAge[0].age
+    );
+    this._state.maxAge = Math.max(
+      menSortedByAge[menSortedByAge.length - 1].age,
+      ladiesSortedByAge[ladiesSortedByAge.length - 1].age
+    );
 
     this.assignSeats();
 
@@ -124,39 +177,30 @@ export class SeatingService {
   }
 
   private assignSeats() {
-    const numberOfTables = this._tables.length;
+    const numberOfMen = this.manQueue.length;
 
     const currentSeatingOfMen: Participant[] = [];
     this.manQueue.forEach((man, index) => {
       const position = index + this.position;
       const tableIndex =
         position < 0
-          ? position + numberOfTables
-          : position >= numberOfTables
-          ? position - numberOfTables
+          ? position + numberOfMen
+          : position >= numberOfMen
+          ? position - numberOfMen
           : position;
       currentSeatingOfMen[tableIndex] = man;
     });
 
-    this.takingABreak = [];
-    for (let i = 0; i < this.numberOfMissingLadies; i++) {
-      const removeAtBeginning = i % 2 === 0;
-      if (removeAtBeginning) {
-        this.takingABreak.push(currentSeatingOfMen.shift() as Participant);
-      } else {
-        this.takingABreak.push(currentSeatingOfMen.pop() as Participant);
-      }
-    }
-
-    this._tables.forEach((table, index) => {
+    this._state.dates.forEach((date, index) => {
       const man = currentSeatingOfMen[index];
       if (man === SeatingService.missingMan) {
-        this.takingABreak.push(table.lady);
+        delete date.man;
       } else {
-        table.man = currentSeatingOfMen[index];
+        date.man = currentSeatingOfMen[index];
       }
     });
-    this.tables.next(this._tables);
+    this._state.dateNumber = this.position - this.startPosition;
+    this.state.next(this._state);
   }
 }
 
